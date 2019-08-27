6f8114037b33e8884bec94e9e84fb97a68462557
SahilShikalgar <shikalgar32@gmail.com>
2019-08-27 18:02:55 +0530

    Shopping-list Changes

--- shopping-cart/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ shopping-cart/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', {static: false}) nameInputReference: ElementRef;
+  @ViewChild('amountInput', {static: false}) amountInputReference: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddItem() {
+    const ingName = this.nameInputReference.nativeElement.value;
+    const ingAmount = this.amountInputReference.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
