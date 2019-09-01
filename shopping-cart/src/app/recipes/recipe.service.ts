6d535a07af073ed886aee617e5de3d5c91b125b0
SahilShikalgar <shikalgar32@gmail.com>
2019-09-01 14:49:26 +0530

    Implemented services for recipe module and cross module communication

--- shopping-cart/src/app/recipes/recipe.service.ts
+++ shopping-cart/src/app/recipes/recipe.service.ts
@@ -1,16 +1,46 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppinglistService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     
     private recipes: Recipe[] = [
-        new Recipe("A Test Recipe 1", "This is simply a test", "https://www.thekitchenmagpie.com/wp-content/uploads/images/2019/07/chickensalad.jpg"),
-        new Recipe("A Test Recipe 2", "This is simply a test", "https://www.thekitchenmagpie.com/wp-content/uploads/images/2019/07/chickensalad.jpg"),
-        new Recipe("A Test Recipe 3", "This is simply a test", "https://www.thekitchenmagpie.com/wp-content/uploads/images/2019/07/chickensalad.jpg")
+        new Recipe(
+            "A Test Recipe 1", 
+            "This is simply a test", 
+            "https://www.thekitchenmagpie.com/wp-content/uploads/images/2019/07/chickensalad.jpg",
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe(
+            "A Test Recipe 2", 
+            "This is simply a test", 
+            "https://www.thekitchenmagpie.com/wp-content/uploads/images/2019/07/chickensalad.jpg",
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe(
+            "A Test Recipe 3", 
+            "This is simply a test", 
+            "https://www.thekitchenmagpie.com/wp-content/uploads/images/2019/07/chickensalad.jpg",
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ])
     ];
+
+    constructor(private slService: ShoppinglistService) { }
     
     getRecipes() {
         return this.recipes.slice();    //return copy of array
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredientsFromRecipe(ingredients);
     }
 }
--- shopping-cart/src/app/shopping-list/shopping-list.component.ts
+++ shopping-cart/src/app/shopping-list/shopping-list.component.ts
@@ -1,22 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppinglistService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styles: []
+  templateUrl: './shopping-list.component.html'
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10)
-  ];
-  constructor() { }
+  ingredients: Ingredient[] = [];
+
+  constructor(private shoppinglistService: ShoppinglistService) { }
 
   ngOnInit() {
-  }
-
-  onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
+    this.ingredients = this.shoppinglistService.getIngredients();
+    this.shoppinglistService.ingredientsChanged
+      .subscribe((ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      })
   }
 }
